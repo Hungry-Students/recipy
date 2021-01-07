213a2e9b541e2bffbf1336ff6d42069d0d349694
grumo <axel.kugelmann@ens.fr>
2021-01-07 16:27:29 +0100

    Add initial models (databases) for Ingredients and Recipes, with the ManyToMany relationship

--- recipy/recipes/models.py
+++ recipy/recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+	ingredient_name = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return self.ingredient_name
+	
+class Recipe(models.Model):
+	recipe_name = models.CharField(max_length=200)
+	ingredients = models.ManyToManyField(Ingredient)
+	
+	def __str__(self):
+		return self.recipe_name
