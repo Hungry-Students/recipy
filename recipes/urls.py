ab96589391f1b881792c6914048a2d2878f12055
grumo <axel.kugelmann@ens.fr>
2021-01-12 16:33:31 +0100

    Add small recipy view

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:recipe_id>/', views.detail_recipe, name='detail_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from django.template import loader
 from .models import Ingredient, RecipeCategory, RestrictedDiet, Recipe
 
 
 def index(request):
 	latest_recipes_list = Recipe.objects.order_by("-id")[:5]
-	template = loader.get_template('recipes/index.html')
 	context = {
 		'latest_recipes_list': latest_recipes_list,
 	}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'recipes/index.html', context)
+	
+def detail_recipe(request, recipe_id):
+	recipe = Recipe.objects.filter(id=recipe_id)[0]
+	context = {
+		'recipe':recipe,
+	}
+	return render(request, 'recipes/detail_recipe.html', context)
