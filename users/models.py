ce021aa9d048da4e99f671a83269ce1ab42afa3a
Jules Saget <jules.saget@ens.fr>
2021-01-26 16:12:36 +0100

    Continue working on user dashboard
    Playing with Django views to learn how to do stuff

--- users/models.py
+++ users/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from recipes.models import Recipe
 
 class User(AbstractUser):
-    pass
+    recipes = models.ManyToManyField(Recipe, through='RecipeDate')
+
+    def __str__(self):
+        return self.username
+
+
+class RecipeDate(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    recipe = models.ForeignKey(Recipe, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.recipe.name
--- users/views.py
+++ users/views.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import User, RecipeDate, Recipe
 
 def dashboard(request):
-    return render(request, 'users/dashboard.html')
+    return render(request, 'users/dashboard.html', context)
+
+def cookbook(request, username):
+    user = User.objects.filter(username=username)[0]
+    user_recipes = user.recipes.all()
+    context = {
+        'recipes': user_recipes,
+    }
+    return render(request, 'TODO', context)
