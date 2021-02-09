ec3f178a356c003ec1c408fa547796ab386410c1
Jules Saget <jules.saget@ens.fr>
2021-02-09 16:19:24 +0100

    Factor recipe display template into a new file

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
+import re
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponseRedirect #, HttpResponse
 from django.urls import reverse
 from recipe_scrapers import scrape_me, WebsiteNotImplementedError
 from .models import Ingredient, Recipe, IngredientQuantity #, RecipeCategory, RestrictedDiet
 from .parser import IngredientParser, YieldsParser
 from .forms import RecipeForm
-import re
 
 def index(request):
     latest_recipes_list = Recipe.objects.order_by('-id')[:5]
@@ -20,103 +17,102 @@ def index(request):
 
 def detail_recipe(request, recipe_id):
     recipe = Recipe.objects.filter(id=recipe_id)[0]
-    ingredient_quantity_list = IngredientQuantity.objects.filter(recipe = recipe_id)
     context = {
         'recipe':recipe,
-        'ingredient_quantities':ingredient_quantity_list,
     }
     return render(request, 'recipes/detail_recipe.html', context)
 
 ### SUBMITTING RECIPES ###
-    
+
 def write(request, error_message_link=None, error_message_form=None, existing_recipe=None):
-	form = RecipeForm(initial = {'quantity_unit' : 'servings'})
-	ingredient_list = Ingredient.objects.order_by('name')
-	context = {
-		'error_message_link' : error_message_link,
-		'error_message_form' : error_message_form,
-		'existing_recipe' : existing_recipe,
-		'form' : form,
-		'ingredient_list' : ingredient_list,
-	}
-	return render(request , 'recipes/write.html', context)
+    form = RecipeForm(initial = {'quantity_unit' : 'servings'})
+    ingredient_list = Ingredient.objects.order_by('name')
+    context = {
+        'error_message_link' : error_message_link,
+        'error_message_form' : error_message_form,
+        'existing_recipe' : existing_recipe,
+        'form' : form,
+        'ingredient_list' : ingredient_list,
+    }
+    return render(request , 'recipes/write.html', context)
 
 ### SUBMITTING VIA A FORM ###
 
 def handle_form(request):
-	form = RecipeForm(request.POST)
-	if form.is_valid():
-	
-    	#checking for duplicates of recipes
-		temp = Recipe.objects.filter(name = form.cleaned_data['name'])
-		if temp:
-		    return write(request, error_message_form = 'this recipe already exists', existing_recipe = temp[0])
-		
-		#extracting ingredients and checking for ingredient duplicates. Note : we do that before saving the recipe to avoid incomplete recipes.
-		ingredient_names = {}
-		for e in request.POST.keys():
-			m = re.search('ingredient(?P<id>[0-9]+)_name', e)
-			if m is not None:
-				ingredient_id = m.group('id')
-				ingredient_name = request.POST[e]
-				if ingredient_name == '':
-				    continue
-				ingredient_name = ingredient_name.lower() #note : we enforce lower case here for ingredient name
-				if ingredient_name in ingredient_names.values():
-				    return write(request, error_message_form = 'You have listed ingredient '+ingredient_name+' several times')
-				ingredient_names[ingredient_id] = ingredient_name
-		
-		r = form.save()
-		
-		#Manage cook time
-		cook_time_hours = request.POST.get('cook_time_hours', 0)
-		if not cook_time_hours:
-			cook_time_hours = 0
-		cook_time_minutes = request.POST.get('cook_time_minutes', 0)
-		if not cook_time_minutes:
-			cook_time_minutes = 0
-			
-		r.cook_time = 60*int(cook_time_hours)+int(cook_time_minutes)
-		r.save()
-		
-		#Parsing quantities and saving ingredients
-		p = IngredientParser()
-		for ingredient_id, ingredient_name in ingredient_names.items():
-			ingredient_quantity, ingredient_unit = p.parse_quantity(request.POST.get('ingredient'+ingredient_id+'_quantity', ''))
-			add_ingredient(r, ingredient_name, ingredient_quantity, ingredient_unit)
-		
-		return HttpResponseRedirect(reverse('recipes:index'))
-	return write(request, error_message_form = 'Something went wrong')
+    form = RecipeForm(request.POST)
+    if form.is_valid():
 
+        #checking for duplicates of recipes
+        temp = Recipe.objects.filter(name = form.cleaned_data['name'])
+        if temp:
+            return write(request, error_message_form = 'this recipe already exists', existing_recipe = temp[0])
+
+        #extracting ingredients and checking for ingredient duplicates. Note : we do that before saving the recipe to avoid incomplete recipes.
+        ingredient_names = {}
+        for e in request.POST.keys():
+            m = re.search('ingredient(?P<id>[0-9]+)_name', e)
+            if m is not None:
+                ingredient_id = m.group('id')
+                ingredient_name = request.POST[e]
+                if ingredient_name == '':
+                    continue
+                ingredient_name = ingredient_name.lower() #note : we enforce lower case here for ingredient name
+                if ingredient_name in ingredient_names.values():
+                    return write(request, error_message_form = 'You have listed ingredient '+ingredient_name+' several times')
+                ingredient_names[ingredient_id] = ingredient_name
+
+        r = form.save()
+
+        #Manage cook time
+        cook_time_hours = request.POST.get('cook_time_hours', 0)
+        if not cook_time_hours:
+            cook_time_hours = 0
+        cook_time_minutes = request.POST.get('cook_time_minutes', 0)
+        if not cook_time_minutes:
+            cook_time_minutes = 0
+
+        r.cook_time = 60*int(cook_time_hours)+int(cook_time_minutes)
+        r.save()
+
+        #Parsing quantities and saving ingredients
+        p = IngredientParser()
+        for ingredient_id, ingredient_name in ingredient_names.items():
+            ingredient_quantity, ingredient_unit = p.parse_quantity(request.POST.get('ingredient'+ingredient_id+'_quantity', ''))
+            add_ingredient(r, ingredient_name, ingredient_quantity, ingredient_unit)
+
+        return HttpResponseRedirect(reverse('recipes:index'))
+    return write(request, error_message_form = 'Something went wrong')
 
 ### SUBMITTING VIA RECIPE-SCRAPPER ###
 
 def scrape(request):
-	try:
-		existing_recipes = Recipe.objects.filter(url = request.POST['url'])
-		if not existing_recipes:
-			scraper = scrape_me(request.POST['url'])
-			yields_parser = YieldsParser()
-			yields_parser.parse(scraper.yields())
-			print(scraper.total_time())
-			new_recipe = Recipe(name = scraper.title(),
-								instruction = scraper.instructions(),
-								quantity = yields_parser.yields,
-								quantity_unit = yields_parser.yields_unit.lower(),
-								url = request.POST['url']
-								)
-			new_recipe.save()
-			ingredient_parser = IngredientParser()
-			for e in scraper.ingredients():
-				ingredient_parser.parse(e.lower()) #note : we enforce lower case in the argument, and will give upper case (e.g. cL) in the parser when needed
-				add_ingredient(new_recipe, ingredient_parser.ingredient_name, ingredient_parser.quantity, ingredient_parser.quantity_unit)
-		else:
-			return write(request, error_message_link = 'this recipe already exists', existing_recipe = existing_recipes[0])
-		return HttpResponseRedirect(reverse('recipes:index'))
-	except WebsiteNotImplementedError:
-		msg = 'the url '+ request.POST['url'] + ' is not supported by recipe_scraper'
-		return write(request, error_message_link=msg)
-		
+    try:
+        existing_recipes = Recipe.objects.filter(url = request.POST['url'])
+        if not existing_recipes:
+            scraper = scrape_me(request.POST['url'])
+            yields_parser = YieldsParser()
+            yields_parser.parse(scraper.yields())
+            print(scraper.total_time())
+            new_recipe = Recipe(name = scraper.title(),
+                                instruction = scraper.instructions(),
+                                quantity = yields_parser.yields,
+                                quantity_unit = yields_parser.yields_unit.lower(),
+                                url = request.POST['url']
+                                )
+            new_recipe.save()
+            ingredient_parser = IngredientParser()
+            for e in scraper.ingredients():
+                ingredient_parser.parse(e.lower())
+                # NOTE: we force lower case in the argument, upper case will be
+                # given in the parser when needed (e.g. cL)
+                add_ingredient(new_recipe, ingredient_parser.ingredient_name, ingredient_parser.quantity, ingredient_parser.quantity_unit)
+        else:
+            return write(request, error_message_link = 'this recipe already exists', existing_recipe = existing_recipes[0])
+        return HttpResponseRedirect(reverse('recipes:index'))
+    except WebsiteNotImplementedError:
+        msg = 'the url '+ request.POST['url'] + ' is not supported by recipe_scraper'
+        return write(request, error_message_link=msg)
+
 ### Non view functions ###
 
 def add_ingredient(recipe, ingredient_name, ingredient_quantity, ingredient_quantity_unit):
@@ -135,4 +131,3 @@ def add_ingredient(recipe, ingredient_name, ingredient_quantity, ingredient_quan
                                   quantity_unit = ingredient_quantity_unit
                                   )
     relation.save()
-
