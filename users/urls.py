200303d96ac705d70758a1d3fc03532f33f51bf4
Jules Saget <jules.saget@ens.fr>
2021-02-09 18:08:50 +0100

    Unify templates

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from users.views import dashboard
 
+app_name='users'
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', dashboard, name='dashboard'),
