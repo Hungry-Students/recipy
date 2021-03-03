1649e7f5d182ab2a76ce2c7ac5171613bb9d4d63
grumo <axel.kugelmann@ens.fr>
2021-03-03 09:44:19 +0100

    Add host and a migration

--- recipy/settings.py
+++ recipy/settings.py
@@ -27,7 +27,7 @@ with open(".secret-key.txt") as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 ACTIVITYPUB_DOMAIN = "localhost"
 
 
